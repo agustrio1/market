a392d35699074941e2896e885f67c5a7e11df2d4
agustrio1 <agustrio1998@gmail.com>
2023-10-13 05:13:11 +0700

    change register

--- client/src/pages/accounts/Register.jsx
+++ client/src/pages/accounts/Register.jsx
@@ -8,9 +8,12 @@ import {
 } from "firebase/auth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const Register = () => {
   const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
   const navigate = useNavigate();
 
   const handleGoogleLogin = () => {
@@ -55,65 +58,75 @@ const Register = () => {
 
   return (
     <main className="w-screen min-h-screen flex flex-col bg-gradient-to-tr from-orange-800 to-orange-500 max-w-[500px] mx-auto p-10 pt-16">
-      <form
-        className="w-full bg-white flex flex-col gap-4 shadow-lg rounded-lg mt-8 p-6"
-        autoComplete="off"
-        onSubmit={handleGoogleRegister}
-      >
-        <h1 className="text-4xl text-orange-500 text-center">Register</h1>
-        {errorMessage && (
-          <p className="text-red-500 text-center">{errorMessage}</p>
-        )}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            className="input-field h-10 rounded-md border-[1px] border-gray-300"
-          />
-        </div>
+    <form
+      className="w-full bg-white flex flex-col gap-4 shadow-lg rounded-lg mt-8 p-6"
+      autoComplete="off"
+      onSubmit={handleGoogleRegister}
+    >
+      <h1 className="text-4xl text-orange-500 text-center">Register</h1>
+      {errorMessage && (
+        <p className="text-red-500 text-center">{errorMessage}</p>
+      )}
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          className="input-field h-10 rounded-md border-[1px] border-gray-300"
+        />
+      </div>
 
-        <div className="flex flex-col gap-2">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            className="input-field h-10 rounded-md border-[1px] border-gray-300"
-          />
-        </div>
+      <div className="flex flex-col gap-2 relative">
+        <label htmlFor="password">Password</label>
+        <input
+          type={showPassword ? "text" : "password"}
+          id="password"
+          className="input-field h-10 rounded-md border-[1px] border-gray-300"
+        />
+        <FontAwesomeIcon
+          icon={showPassword ? faEye : faEyeSlash}
+          className="absolute top-3 right-3 cursor-pointer text-gray-500"
+          onClick={() => setShowPassword(!showPassword)}
+        />
+      </div>
 
-        <div className="flex flex-col gap-2">
-          <label htmlFor="password2">Ulangi Password</label>
-          <input
-            type="password"
-            id="password2"
-            className="input-field h-10 rounded-md border-[1px] border-gray-300"
-          />
-        </div>
+      <div className="flex flex-col gap-2 relative">
+        <label htmlFor="password2">Ulangi Password</label>
+        <input
+          type={showPassword2 ? "text" : "password"}
+          id="password2"
+          className="input-field h-10 rounded-md border-[1px] border-gray-300"
+        />
+        <FontAwesomeIcon
+          icon={showPassword2 ? faEye : faEyeSlash}
+          className="absolute top-3 right-3 cursor-pointer text-gray-500"
+          onClick={() => setShowPassword2(!showPassword2)}
+        />
+      </div>
 
-        <div className="mt-4 flex flex-col gap-2">
-          <button
-            className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded"
-            type="submit"
-          >
-            Register
-          </button>
-          <button
-            className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded"
-            type="button"
-            onClick={handleGoogleLogin}
-          >
-            <FontAwesomeIcon icon={faGoogle} className="mr-2" /> Gunakan Google
-          </button>
-          <Link
-            to={"/login"}
-            className="bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded flex justify-center items-center"
-          >
-            Login
-          </Link>
-        </div>
-      </form>
-    </main>
+      <div className="mt-4 flex flex-col gap-2">
+        <button
+          className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Register
+        </button>
+        <button
+          className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded"
+          type="button"
+          onClick={handleGoogleLogin}
+        >
+          <FontAwesomeIcon icon={faGoogle} className="mr-2" /> Gunakan Google
+        </button>
+        <Link
+          to={"/login"}
+          className="bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded flex justify-center items-center"
+        >
+          Login
+        </Link>
+      </div>
+    </form>
+  </main>
   );
 };
 
