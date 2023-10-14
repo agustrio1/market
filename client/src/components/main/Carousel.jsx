d66e54e67fc4d42f612caafd5ae68639f0594a7c
agustrio1 <agustrio1998@gmail.com>
2023-10-15 05:26:02 +0700

    change img carousel

--- client/src/components/main/Carousel.jsx
+++ client/src/components/main/Carousel.jsx
@@ -4,14 +4,13 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const CarouselImg = () => {
   const carouselImg = [
-    { src: '../slide-img/slide-1.png', alt: 'Image 1' },
-    { src: '../slide-img/slide-2.png', alt: 'Image 2' },
-    { src: '../slide-img/slide-3.png', alt: 'Image 3' },
-    { src: '../slide-img/slide-4.png', alt: 'Image 4' },
+    { src: '../slide-img/slide-1.jpg', alt: 'Image 1' },
+    { src: '../slide-img/slide-2.jpg', alt: 'Image 2' },
+    { src: '../slide-img/slide-3.jpg', alt: 'Image 3' },
   ];
 
   return (
-    <div className="relative z-0 mb-16">
+    <div className="relative z-0 mb-8">
       <Carousel
         showArrows={true}
         showStatus={false}
@@ -26,7 +25,7 @@ const CarouselImg = () => {
             <img
               src={image.src}
               alt={image.alt}
-              className="w-25 h-80"
+              className="w-full h-auto"
             />
           </div>
         ))}
