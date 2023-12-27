c69f554c225e98e43b2d6519a02ec0d3230622b5
agustrio1 <susantoagustrio16@gmail.com>
2023-12-27 19:09:18 +0700

    update

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3LeVdaBvo4u8dldn3BKvG-3CXzPD55ds",
+  authDomain: "web-store-8304e.firebaseapp.com",
+  databaseURL: "https://web-store-8304e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "web-store-8304e",
+  storageBucket: "web-store-8304e.appspot.com",
+  messagingSenderId: "120453699927",
+  appId: "1:120453699927:web:3b697f65970be434a24612",
+  measurementId: "G-76NYC96CZ2"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
